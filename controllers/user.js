eeb0e8d4ef3ea7b3f4fe96f6f0d72b8c00fc650a
yuvraj singh <yuvrajsingh020814@gmail.com>
2024-12-12 18:29:42 +0530

    JWT token

--- controllers/user.js
+++ controllers/user.js
@@ -19,9 +19,9 @@ async function handleUserLogin(req,res){
         error: "Invalid Username or password",
     });
 
-    const sessionId = uuidv4();
-    setUser(sessionId, user);
-    res.cookie("uid", sessionId);
+    
+   const token = setUser(user);
+    res.cookie("uid", token);
     return res.redirect("/");
 }
 
--- services/auth.js
+++ services/auth.js
@@ -1,14 +1,27 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "yuvrj@singh";
 
-function setUser(id, user) {
-    sessionIdToUserMap.set(id, user);
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-function getUser(id) {
-    return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
+
 }
 
-module.exports ={
-    setUser,
-    getUser,
-}
+module.exports = {
+  setUser,
+  getUser,
+};
